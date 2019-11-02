f038dc46f40d5ce5e7f1acab549045b33b7f830e
ljliujyun <ljliujyun@gmail.com>
2019-11-02 13:51:18 +0000

    script now fetch files

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+console.log('started script')
 let d = new Date();
-document.getElementById('date').innerHTML = "<h1>Today's date is " + d + "</h1>"
+document.getElementById('date').innerHTML += "<p>Today's date is " + d + "</p>"
+
+
+async function importone(date) {
+    const response = await fetch(date+'.txt');
+    const myJson = await response.text();
+    const string = String(myJson);
+    const text = '<h2>'+ date +'</h2>' + '<p>' + string.replace(/\r\n/g,'</p><p>') + '</p>';
+    console.log(string.split());
+
+    document.getElementsByClassName('column mid').item(0).innerHTML += text;
+
+}
+
+async function importtext() {
+    const response = await fetch('catalogue.txt');
+    const myJson = await response.text();
+    const string = String(myJson);
+    const array =  string.split(/\r\n/);
+    array.forEach(element => {
+        importone(element);
+    });
+}
+
+
+importtext()
+
+console.log('finished script')
