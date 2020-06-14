a47902dec3098ade63592c4e5a3afbf4e81a7279
ljliujyun <ljliujyun@gmail.com>
2020-06-14 22:19:27 +0800

    first diary entry

--- books_script.js
+++ books_script.js
@@ -3,7 +3,7 @@ console.log('started script')
 
 async function import_one(element) {
     if (element[1] !== 'hide') {
-        const response = await fetch('diary_entries/'+ element[0] + '.txt');
+        const response = await fetch('books_entries/'+ element[0] + '.txt');
         const myJson = await response.text();
         const string = String(myJson);
         const text = '<h2>' + element[0] + '</h2>' + '<p>' + string.replace(/\r\n|\r|\n/g, '</p><p>') + '</p>';
@@ -12,7 +12,7 @@ async function import_one(element) {
 }
 
 async function import_text() {
-    const response = await fetch('diary_catalogue.txt');
+    const response = await fetch('books_catalogue.txt');
     const myJson = await response.text();
     const string = String(myJson);
     const array = string.split(/\r\n|\r|\n/).map(atom => atom.split('_'));
