c5e5e0a62646416c322feef33e55f88b42b969a2
ljliujyun <ljliujyun@gmail.com>
2020-06-14 22:11:14 +0800

    big changes - added music and diary_entries folder and books nav

--- diary_script.js
+++ diary_script.js
@@ -3,7 +3,7 @@ console.log('started script')
 
 async function import_one(element) {
     if (element[1] !== 'hide') {
-        const response = await fetch(element[0] + '.txt');
+        const response = await fetch('diary_entries/'+ element[0] + '.txt');
         const myJson = await response.text();
         const string = String(myJson);
         const text = '<h2>' + element[0] + '</h2>' + '<p>' + string.replace(/\r\n|\r|\n/g, '</p><p>') + '</p>';
