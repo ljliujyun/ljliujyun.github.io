d56217902af6ddac290e65ec37a17b949d71ef71
ljliujyun <ljliujyun@gmail.com>
2020-06-20 15:36:07 +0800

    book entry my minimalistic life minor corrections

--- diary_script.js
+++ diary_script.js
@@ -6,7 +6,7 @@ async function import_one(element) {
         const response = await fetch('diary_entries/'+ element[0] + '.txt');
         const myJson = await response.text();
         const string = String(myJson);
-        const text = '<h2>' + element[0] + '</h2>' + '<p>' + string.replace(/\r\n|\r|\n/g, '</p><p>') + '</p>';
+        const text = '<h2>< ' + element[0] + '></h2>' + '<p>' + string.replace(/\r\n|\r|\n/g, '</p><p>') + '</p>';
         return text
     }
 }
