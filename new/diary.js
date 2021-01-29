7673a0dc45c2622414891829291c25d28d56190e
jasonchanghf <jasonchanghf@gmail.com>
2021-01-29 17:59:59 +0800

    testing

--- new/diary.js
+++ new/diary.js
@@ -1,19 +1,33 @@
-async function import_text() {
-    const response = await fetch('nav.html');
+
+async function import_text(name) {
+    const response = await fetch('diary_entries/'+name);
     console.log(response);
     const myJson = await response.text();
     const string = String(myJson);
     document.getElementById('content').innerHTML = string;
+    console.log('end');
+    
+}
+async function import_nav() {
+    const response = await fetch('nav.html');
+    console.log(response);
+    const myJson = await response.text();
+    const string = String(myJson);
+    document.getElementById('nav').innerHTML = string;
     console.log('hi')
 
     document.querySelectorAll('.nav').forEach(item => {
         console.log(item);
         item.addEventListener('click', event => {
           console.log(item.innerHTML);
+          import_text(item.innerHTML)
         })
       })
+    
     
 }
 
 
-import_text()
+import_nav()
+
+
